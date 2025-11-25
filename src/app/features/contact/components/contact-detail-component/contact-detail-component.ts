f659dad79887e9bbfdfab964267fc1313fb22c04
Rahul mishra <rahul.mi8750@gmail.com>
2025-11-25 23:33:47 +0530

    Few ui changes added

--- src/app/features/contact/components/contact-detail-component/contact-detail-component.ts
+++ src/app/features/contact/components/contact-detail-component/contact-detail-component.ts
@@ -1,11 +1,38 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { TextareaModule } from 'primeng/textarea';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-contact-detail-component',
-  imports: [],
+  imports: [
+    CommonModule,
+    InputTextModule,
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    TextareaModule,
+    ButtonModule,
+  ],
   templateUrl: './contact-detail-component.html',
-  styleUrl: './contact-detail-component.scss'
+  styleUrl: './contact-detail-component.scss',
 })
 export class ContactDetailComponent {
-
+  detailData = [
+    {
+      label: 'Office',
+      description: '1234 N Spring St, Los Angeles, CA 90012, United States.',
+    },
+    {
+      label: 'Phone',
+      description: '+91 - 89202897174',
+    },
+    {
+      label: 'Email',
+      description: 'rahul.mi8920@gmail.com',
+    },
+  ];
 }
